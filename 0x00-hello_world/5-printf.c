0a9060bc4720aff0c40aec203e3f0a4e0e4114c5
tracwov <wovenberries2gmail.com>
2023-06-16 05:40:36 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program the prints a line using the printf function
- * return (0) (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+return (0);
 }
