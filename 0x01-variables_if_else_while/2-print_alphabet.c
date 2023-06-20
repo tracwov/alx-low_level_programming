ef430a874c30b4a3d7013766026bb939402d1e15
tracwov <wovenberries2gmail.com>
2023-06-19 21:59:21 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Description: 'print alphabets lowercase'
+ *Return: Aways 0
+ */
+int main(viod)
+{
+	int n = 97;
+
+	while (n <= 122)
+{
+	putchar(n);
+	n++
+}
+	return (0);
+}
