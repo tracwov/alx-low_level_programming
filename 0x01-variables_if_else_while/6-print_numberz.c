3e012b890835eb98d472a156f2b51167acde62e6
tracwov <wovenberries2gmail.com>
2023-06-20 08:26:07 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  * Description: 'print numbers of base 10 using putchar'
- * Return always 0
+ * Return: always 0
  */
 int main(void)
 {
 	int n;
-	
+
 	for (n = 0; n <= 9; n++)
 	{
 		putchar(n + 48);
