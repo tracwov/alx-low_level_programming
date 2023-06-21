0335330133587338a0db80327f2addcf7562ab5f
tracwov <wovenberries2gmail.com>
2023-06-20 22:48:50 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
-        for (j = 1; i <= 10; i++)
-        {
-                for (j = 97; j <= 122; j++)
-                {
-                        _putchar(j);
-                }
-                _putchar('\n');
-        }
+	for (j = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
