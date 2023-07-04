081a8c77f3bf56bacc0c69ea5805f19dfd4e7d25
tracwov <wovenberries2gmail.com>
2023-07-03 23:16:11 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,7 @@
  *
  * Return: A pointer to the destination buffer @dest.
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(void *dest, char *src, size_t n)
 {
 	unsigned int index;
 	unsigned char *destination = dest;
