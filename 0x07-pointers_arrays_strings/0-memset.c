8e5ffc8fe2fd52eb603bb0fe391347c80c36e10c
tracwov <wovenberries2gmail.com>
2023-07-03 23:38:13 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,7 +9,7 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
